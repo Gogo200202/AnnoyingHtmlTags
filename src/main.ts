d9a485fd5fd4f7d92debf67f837da0c09f57de20
Gogo200202 <gosheto420@gmail.com>
2024-03-09 15:31:12 +0200

    add css folder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 const rangeBirth=document.getElementById("birthDate")as HTMLInputElement;
 const resultOfBirthDate= document.getElementById("resultOfBirthDate")as HTMLInputElement;
-console.log(rangeBirth);
 rangeBirth.addEventListener("input",(e:Event)=>{
      let target=e.target as HTMLInputElement;
      resultOfBirthDate.innerHTML=target.value;
 })
+
+
 
